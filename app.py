1a918bf66ad2fbf8488c574b5e35cb9fcc4ebdb6
Ghislain Byimbo <byimbog250@gmail.com>
2025-09-02 21:49:25 +0200

    4 commit

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ FILE = "expenses.json"
 # Load previous expenses
 def load_expenses():
     try:
-       
+        with open(FILE, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
 
 
 
