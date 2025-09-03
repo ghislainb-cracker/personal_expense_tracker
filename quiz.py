7f216ee1197b706a867567720ee16c04262290fb
Ghislain Byimbo <byimbog250@gmail.com>
2025-09-03 23:47:45 +0200

    26 commit

--- quiz.py
+++ quiz.py
@@ -4,7 +4,8 @@ def run_quiz(questions):
     score = 0
     for q in questions:
         print("\n" + q["question"])
-        
+        for i, option in enumerate(q["options"], start=1):
+            print(f"{i}. {option}")
         
        
 
