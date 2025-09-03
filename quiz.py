2d48df6d2f2c3d61488fee3983637579598f6c3c
Ghislain Byimbo <byimbog250@gmail.com>
2025-09-03 23:47:52 +0200

    27 commit

--- quiz.py
+++ quiz.py
@@ -6,7 +6,10 @@ def run_quiz(questions):
         print("\n" + q["question"])
         for i, option in enumerate(q["options"], start=1):
             print(f"{i}. {option}")
-        
+        answer = input("Your answer (1-4): ")
+
+        if answer.isdigit() and int(answer) == q["answer"]:
+            print("✅ Correct!")
        
 
 
