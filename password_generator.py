051847257aba18ec4ef034babeb403dc75baced3
Ghislain Byimbo <byimbog250@gmail.com>
2025-09-05 23:31:26 +0200

    48 commit

--- password_generator.py
+++ password_generator.py
@@ -5,7 +5,7 @@ def generate_password(length=12):
     # characters to use
     characters = string.ascii_letters + string.digits + string.punctuation
     # generate password
-    
+    password = ''.join(random.choice(characters) for _ in range(length))
     
 
 
