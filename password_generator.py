27bae99a4c33f7a21328e6c32c8b3e2d583f41d5
Ghislain Byimbo <byimbog250@gmail.com>
2025-09-05 23:33:15 +0200

    60 commit

--- password_generator.py
+++ password_generator.py
@@ -2,9 +2,9 @@ import random
 import string
 
 def generate_password(length=12):
-    # characters to use
+    
     characters = string.ascii_letters + string.digits + string.punctuation
-    # generate password
+   
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
 
